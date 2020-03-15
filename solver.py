12cab5fa9abb7720421c47c8824a5366aef54c4e
Brandon Hawi <brandonhawi@jasonbusiness.com>
2020-03-14 23:23:37 -0700

    Finished general solver

---
+++ solver.py
@@ -0,0 +1,97 @@
+# board = [[5, 3, 0, 0, 7, 0, 0, 0, 0],
+#          [6, 0, 0, 1, 9, 5, 0, 0, 0],
+#          [0, 9, 8, 0, 0, 0, 0, 6, 0],
+#          [8, 0, 0, 0, 6, 0, 0, 0, 3],
+#          [4, 0, 0, 8, 0, 3, 0, 0, 1],
+#          [7, 0, 0, 0, 2, 0, 0, 0, 6],
+#          [0, 6, 0, 0, 0, 0, 2, 8, 0],
+#          [0, 0, 0, 4, 1, 9, 0, 0, 5],
+#          [0, 0, 0, 0, 8, 0, 0, 7, 9]]
+
+board = [[0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,3,0,8,5],
+        [0,0,1,0,2,0,0,0,0],
+        [0,0,0,5,0,7,0,0,0],
+        [0,0,4,0,0,0,1,0,0],
+        [0,9,0,0,0,0,0,0,0],
+        [5,0,0,0,0,0,0,7,3],
+        [0,0,2,0,1,0,0,0,0],
+        [0,0,0,0,4,0,0,0,9]]
+
+def printBoard():
+    print("-----------------")
+    for row in board:
+        for col in row:
+            print(col, end=" ")
+        print()
+    print("-----------------")
+
+
+def isValid(row, col, val):
+    return isValidRow(row, val) and isValidCol(col, val) and isValidBox(row, col, val)
+
+
+def isValidRow(row, val):
+    for col in range(len(board[row])):
+        if val == board[row][col]:
+            return False
+    return True
+
+
+def isValidCol(col, val):
+    for row in range(9):
+        if val == board[row][col]:
+            return False
+    return True
+
+
+def isValidBox(row, col, val):
+    rowBox = row // 3
+    colBox = col // 3
+
+    rowIndexes = rowBox*3
+    colIndexes = colBox*3
+
+    for i in range(rowIndexes, rowIndexes+3):
+        for j in range(colIndexes, colIndexes + 3):
+            if board[i][j] == val:
+                return False
+    return True
+
+
+def solve(row, col):
+    for posValue in range(1, 10):
+        if(isOpen(row, col)):
+            if isValid(row, col, posValue):
+                board[row][col] = posValue
+                #printBoard()
+                if row+1 == 9 and col+1 == 9:
+                    return True
+                elif col+1 == 9:
+                    if solve(row+1, 0):
+                        return True
+                    else:
+                        board[row][col] = 0
+                else:
+                    if solve(row, col+1):
+                        return True
+                    else:
+                        board[row][col] = 0
+        else:
+            if row + 1 == 9 and col + 1 == 9:
+                return True
+            elif col + 1 == 9:
+                return solve(row+1, 0)
+            else:
+                return solve(row, col+1)
+    return False
+
+
+def isOpen(row, col):
+    return board[row][col] == 0
+
+
+if __name__ == "__main__":
+    #printBoard()
+    solve(0, 0)
+    printBoard()
